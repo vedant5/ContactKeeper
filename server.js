c57fffaa37367cd30c7e13cb0dd02b21ffe75c83
vedant5 <mathevedant@gmail.com>
2020-06-20 17:26:05 +0530

    prepared to deploy

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 connectDB();
 
 //init middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({ extended: false }));
 
-app.get('/', (req,res)=> res.json({ msg : 'welcome to contact keeper API' }));
+// app.get("/", (req, res) => res.json({ msg: "welcome to contact keeper API" }));
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
